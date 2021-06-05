148514928f62c97e939967f9cf8f64e890fc353b
crisfcodes <cristianflorez0501@gmail.com>
2021-06-04 21:30:55 -0500

    feat(eslint) add promise, comments, and react-native eslint plugins

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
   extends: [
     require.resolve('eslint-config-crisfcodes'),
     require.resolve('eslint-config-crisfcodes/jest'),
+    require.resolve('eslint-config-crisfcodes/promise'),
+    require.resolve('eslint-config-crisfcodes/comments'),
     ifAnyDep('react', require.resolve('eslint-config-crisfcodes/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-crisfcodes/react')),
-    ifAnyDep('react', require.resolve('eslint-config-crisfcodes/promise')),
-    ifAnyDep('react', require.resolve('eslint-config-crisfcodes/comments')),
+    ifAnyDep('react', require.resolve('eslint-config-crisfcodes/react-native')),
   ].filter(Boolean),
   rules: {},
 }
