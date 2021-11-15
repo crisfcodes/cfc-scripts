6903457649fee49cd8384de225067feee6dd14d2
Cristian Flórez <cristianflorez0501@gmail.com>
2021-11-15 01:13:16 -0500

    feat(eslint): add import rules

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -3,8 +3,8 @@ const {ifAnyDep} = require('../utils');
 module.exports = {
   extends: [
     require.resolve('eslint-config-crisfcodes'),
-    require.resolve('eslint-config-crisfcodes/import'),
     require.resolve('eslint-config-crisfcodes/jest'),
+    require.resolve('eslint-config-crisfcodes/import'),
     require.resolve('eslint-config-crisfcodes/promise'),
     require.resolve('eslint-config-crisfcodes/comments'),
     ifAnyDep('react', require.resolve('eslint-config-crisfcodes/jsx-a11y')),
