eb73af1bbaf4f2075a468abe00562d3888e5368a
Cristian Flórez <cristianflorez0501@gmail.com>
2021-11-15 01:05:20 -0500

    feature: add eslint import plugin rules

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -3,6 +3,7 @@ const {ifAnyDep} = require('../utils');
 module.exports = {
   extends: [
     require.resolve('eslint-config-crisfcodes'),
+    require.resolve('eslint-config-crisfcodes/import'),
     require.resolve('eslint-config-crisfcodes/jest'),
     require.resolve('eslint-config-crisfcodes/promise'),
     require.resolve('eslint-config-crisfcodes/comments'),
