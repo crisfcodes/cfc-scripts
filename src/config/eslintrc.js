e4ac01f03e6cfbf7a2800902347b1550d88b1bfc
Cristian Flórez <cristianflorez0501@gmail.com>
2022-01-10 19:27:02 -0500

    fix: update deps

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -3,10 +3,11 @@ const {ifAnyDep} = require('../utils');
 module.exports = {
   extends: [
     require.resolve('eslint-config-crisfcodes'),
-    require.resolve('eslint-config-crisfcodes/jest'),
     require.resolve('eslint-config-crisfcodes/import'),
+    require.resolve('eslint-config-crisfcodes/jest'),
     require.resolve('eslint-config-crisfcodes/promise'),
     require.resolve('eslint-config-crisfcodes/comments'),
+    require.resolve('eslint-config-crisfcodes/filenames'),
     ifAnyDep('react', require.resolve('eslint-config-crisfcodes/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-crisfcodes/react')),
     ifAnyDep(
